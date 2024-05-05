67d3637bef9a463406008ed0d62c8dfdad006bd8
youngbinary <youngbin@barogo.com>
2024-05-06 00:20:23 +0900

    백준 1주차 문제 복습

---
+++ src/main/kotlin/_1주차/기초수학/BG_2004_조합0의개수.java
@@ -0,0 +1,40 @@
+package _1주차.기초수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+
+/**
+ * 백준 2004 조합0 의 개수
+ * https://www.acmicpc.net/problem/2004
+ *
+ * nCr = n! / r!(n-r)!
+ */
+public class BG_2004_조합0의개수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        long n = Long.parseLong(str[0]);
+        long r = Long.parseLong(str[1]);
+
+        long result = fac(n) / (fac(r) * fac(n-r));
+
+        // 소수점 이하 자리수 없는 포맷 설정
+        DecimalFormat decimalFormat = new DecimalFormat("#");
+        char[] chars = decimalFormat.format(result).toCharArray();
+
+        int cnt = 0;
+        for(int i = chars.length-1; i >= 0; i--) {
+            if('0' != chars[i]) break;
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+
+    private static long fac(long a) {
+        if(a == 0) return 1;
+        return a * fac(a-1);
+    }
+}
---
+++ src/main/kotlin/_1주차/문자열/BG_20291_파일정리.java
@@ -0,0 +1,39 @@
+package _1주차.문자열;
+
+import java.io.*;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * 백준 20291 파일정리
+ * https://www.acmicpc.net/problem/20291
+ *
+ * TreeMap 을 이용하면 key 를 사전순으로 정렬해준다.
+ *
+ * 시간:
+ * - System.out.print 사용하면 1488ms
+ * - BufferedWriter 를 사용하면 832ms (메모리: 54936kb)
+ * - StringBuilder 로 출력값을 만든 후 출력하면 780ms (메모리: 65428kb)
+ * - split 대신 subString 사용했을 경우 656ms
+ */
+public class BG_20291_파일정리 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        TreeMap<String, Integer> map = new TreeMap<>();
+
+        int n = Integer.parseInt(br.readLine());
+        while(n-->0) {
+            String file = br.readLine();
+            String ext = file.substring(file.indexOf('.') + 1);
+            if(map.containsKey(ext)) map.put(ext, map.get(ext)+1);
+            else map.put(ext, 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry<String, Integer> m : map.entrySet()) {
+            sb.append(m.getKey()).append(" ").append(m.getValue()).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
