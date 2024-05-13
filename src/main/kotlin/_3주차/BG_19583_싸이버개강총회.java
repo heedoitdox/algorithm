ac7363641042a6eb9723a92c548e53741c568a2c
youngbinary <youngbin@barogo.com>
2024-05-13 22:32:55 +0900

    4주차 필수 1940 문제 풀이 및 오답 노트 작성

---
+++ src/main/kotlin/_3주차/BG_19583_싸이버개강총회.java
@@ -0,0 +1,48 @@
+package _3주차;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * 백준 19583 싸이버개강총회
+ * https://www.acmicpc.net/problem/19583
+ *
+ * 백준에서는 통과하는데 로컬에서 안됨(???????)
+ */
+public class BG_19583_싸이버개강총회 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String S = st.nextToken();
+        String E = st.nextToken();
+        String Q = st.nextToken();
+
+        Set<String> before = new HashSet<>();
+        Set<String> after = new HashSet<>();
+        Set<String> nameSet = new HashSet<>();
+        String str = null;
+
+        while((str = br.readLine()) != null) {
+            String[] arr = str.split(" ");
+            String time = arr[0];
+            String name = arr[1];
+
+            nameSet.add(name);
+            if(S.compareTo(time) >= 0) {
+                before.add(name);
+            }else if(E.compareTo(time) <= 0 && Q.compareTo(time) >= 0) {
+                after.add(name);
+            }
+        }
+
+        int ans = 0;
+        for(String name : nameSet) {
+            if(before.contains(name) && after.contains(name)) {
+                ans += 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
